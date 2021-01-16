fe99c221d71f551089ab8f769eea80f3bbe54e44
unknown <liter@139.com>
2021-01-16 13:25:32 +0800

    20210116-1325

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -31,6 +31,10 @@
       </a-input>
     </a-form-item>
     <a-form-item>
+
+      <a-button type="primary" html-type="submit" class="login-form-button">
+        登 录
+      </a-button>
       <a-checkbox
         v-decorator="[
           'remember',
@@ -39,22 +43,25 @@
             initialValue: true,
           },
         ]"
+        style="margin-right:50px"
       >
         保存密码
       </a-checkbox>
       <a class="login-form-forgot" href="">
         忘记密码
       </a>
-      <a-button type="primary" html-type="submit" class="login-form-button">
-        登 录
-      </a-button>
       或者
       <a href="">
         马上注册!
       </a>
     </a-form-item>
+    <a-form-item>
+        <h4 class="login-tt2">基建管理平台EMS ©2021</h4>
+    </a-form-item>
   </a-form>
+
   </div>
+  
 </template>
  
 <script>
@@ -99,14 +106,6 @@ export default {
 </script>
 
 <style>
-
-#login-form {
-  width: 565px;
-  height: 372px;
-  margin: 0 auto;
-  /* background: url("../assets/houTaiKuang.png"); */
-  padding: 40px 110px;
-}
 /*背景*/
 .login-container {
   position: absolute;
@@ -115,18 +114,37 @@ export default {
   /* //background: url("../assets/houTaiBg.png"); */
 }
 
+#login-form {
+  width: 100%;
+  //height: 100%;
+  max-width: 300px;
+  margin: 0 auto;
+  /* background: url("../assets/houTaiKuang.png"); */
+  padding: 0 auto;
+}
+
 /* Log */
 .login-title {
   color: rgb(0, 0, 0);
   text-align: center;
-  margin: 100px 0;
+  margin: 100px 0px 60px 0px;
   font-size: 48px;
   font-family: Microsoft Yahei;
 }
+
+.login-tt2 {
+  color: rgb(119, 4, 250);
+  text-align: center;
+  margin: 150px 0;
+  vertical-align: bottom;
+  font-size: 16px;
+  font-family: Microsoft Yahei;
+}
+
 /* 登陆按钮 */
-.submit{
+.login-form-button{
   width: 100%;
-  height: 45px;
+  height: 35px;
   font-size: 16px;
 }
 /* 用户登陆标题 */
@@ -143,6 +161,6 @@ export default {
 }
 /* 输入框内左边距50px */
 .ant-input-affix-wrapper .ant-input:not(:first-child) {
-    padding-left: 50px;
+    padding-left: 30px;
 }
 </style>
